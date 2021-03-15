182146314b8e185ce164bdc258c0941c987eab8b
ligiastalves <ligia.stalves@gmail.com>
2021-03-15 16:08:09 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario()
+{ 
+  if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value !="")
+  {
+    alert("Prontinho! Você receberá as novidades por email.")
+  }
+  else{
+    alert("Por favor, preencha os campos nome, email e telefone.")
+  }
+} 
